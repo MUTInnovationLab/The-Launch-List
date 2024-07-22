720167d6cc5340f0e8d32c3315cb577917813ec5
Nkazi01 <nkazimulomongezi72@gmail.com>
2024-07-22 15:56:24 +0200

    map

--- js/app.js
+++ js/app.js
@@ -133,6 +133,34 @@ function displayCategories(categories) {
     addCategoryEventListeners();
 }
 
+
+document.getElementById('contact-forms').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Collect the form data
+    const formData = {
+      from_name:  document.getElementById('name').value,
+      from_email:  document.getElementById('email').value,
+      message: document.getElementById('message').value,
+    };
+
+    // Send the form data using EmailJS
+    emailjs.send('scholarship.website', 'template_diu7o6t', formData)
+        .then(function(response) {
+
+            console.log('Email sent successfully:', response);
+            alert('Your message has been sent successfully!');
+          
+            document.getElementById('contact-form').reset();
+        }, function(error) {
+            console.error('Failed to send email:', error);
+            alert('Oops! An error occurred, please try again later.');
+        });
+});
+
+
+
+
 function addCategoryEventListeners() {
     document.querySelectorAll('.category-filter .btn-outline-primary').forEach(button => {
         button.addEventListener('click', (event) => {
