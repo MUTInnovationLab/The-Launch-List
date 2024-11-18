12ff52f81067140c75e4e9e8355cde1d5d63b2b1
Nkosi <nkosiphila901@gmail.com>
2024-11-18 11:42:58 +0200

    p

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./login.html",
+    "./signup.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
